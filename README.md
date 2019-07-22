4285ada7531a44533591f08c8e9ba2bea9923a3f
tungyao <1922527051@qq.com>
2019-07-22 19:50:05 +0800

    增加自定义姓氏

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 # 不要用windows自带的编辑器
 ## 功能说明
 1. 大规模生成名字,总有一个你满意
-3. 使用自定义的名字生成内容,只需要建立`shici.txt`文件,放入你中意的诗词，而不需要删除符号
+2. 使用自定义的名字生成内容,只需要建立`shici.txt`文件,放入你中意的诗词，而不需要删除符号
+3. 自定义姓氏,只需要加上`-first `
 ## 下载
 [点击下载](https://github.com/tungyao/createname/releases)`https://github.com/tungyao/createname/releases`
 ## 运行方式 ：
 1. 双击运行
-2. cmd : `name.exe`  参数 ：`-number 100 (需要多少名字,默认100) -length 3 (几位的名字,默认3位)`
+2. cmd : `name.exe`  参数 ：`-number 100 (需要多少个名字,默认100) -length 3 (几位的名字,默认3位) -first 李 (默认 周)`
 3. 如果在当前目录下有`shici.txt`文件存在,将使用里面的内容
 ## 运行结果
 会在当前目录生成 `name.txt`
--- name.go
+++ name.go
@@ -1,67 +1,77 @@
 package main
+
 import (
-	"math/rand"
-	"fmt"
-	"time"
-	"sync"
 	"flag"
-	"os"
+	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"regexp"
+	"sync"
+	"time"
 )
+
 var (
-	//定义一堆姓氏
-	familyNames = []string{"周"}
-	
-	//创建辈分字典，姓氏为键，该姓氏的全部【辈分字切片】为值
+	familyNames    = []string{"周"}
 	middleNamesMap = map[string][]string{}
-	
-	//定义一堆名字
-	names = "纤云弄巧飞星传恨银迢迢暗风玉露一相逢胜间无数饮醒复家息雷应倚杖听江声长此何时忘却营阑风静纹平小舟从此逝江海寄余生梦"+
-	"后楼台高锁帘幕年春恨却时落花立微雨双生若只如初见何事秋风画胜寻芳泗水滨无边光景一时新等闲识得风千春江潮水连海平海明月共潮生随波千"+
-	"万里何处春江无月明江流宛转绕芳甸月照花林皆似霰空里流霜觉飞汀白沙看见江天一色无纤尘皎皎空中孤月轮江畔何初见月江月何年初照生代"+
-	"无已江月年只相似知江月待何但见长江送流水白云一去悠悠青枫浦谁家今扁舟子何相思明月楼可楼月徘徊应照离妆镜台玉户帘中卷去衣砧拂还"+
-	"此时相望相闻愿逐月华流照君鸿长飞光鱼潜跃水成文昨闲潭梦落花可春半还家江水流春去欲尽江潭落月复西斜斜月沉沉藏海雾碣石潇湘无限路知乘"+
-	"月几归落月摇情满江树院深几许杨柳烟帘幕无数玉勒鞍游冶楼高见章台路雨风三月暮门掩黄昏无计留春住泪眼问花花语乱飞过秋千去"+
-	"秋风清秋月明落叶聚还散寒复惊相思相见知何此时此难为情入我相思门知我相思苦长相思兮长相忆相思兮无极早知如此绊心何如当初莫相识"+
-	"天门中断楚江开碧流回两岸青山相对出帆明月出天山苍茫云海间长风几万里吹玉门关白登道胡青海由来征战地见有还客望边邑思归多苦颜望边楼未应闲"+
-	"中岁颇好道晚家南山陲来每独往胜事空自知行到水穷坐看云起时偶然值林叟笑无还期新丰美酒斗十千阳游年相逢意气为君饮系马高楼垂柳边出身仕汉羽林郎"+
-	"初随骠战渔阳知向边庭苦纵死犹闻侠香一身能两弧千只似无偏坐金鞍调白羽纷纷射五单于家君臣欢宴终高议云台论战功天子临轩赐侯印将佩明"+
-	"积雨空林烟火迟炊黍饷东漠水田飞白鹭阴阴夏木啭黄鹂山中习静观朝槿松下清斋折露葵与席何事更相疑尊前拟把归期说欲语春容先惨咽生自是有情痴此恨关风与月离歌且莫翻新阕一曲结直须看尽洛城花始共春风容易别"+
-	"驿外断桥边寂寞开无主已是黄昏独自愁更著风和雨无意苦争春一任群芳妒零落成泥碾作尘只有香如故早岁那知世事艰中原北望气如山楼船夜雪瓜洲渡铁马秋风大散关塞上长城空自许镜中衰鬓已先斑出师一表真名世千载谁堪伯仲"+
-	"红藕香残玉簟秋轻解罗裳独上兰舟云中谁寄锦书来雁字回月满西楼花自飘零水自流一种相思两闲愁此情无计可消除才下眉头却上心头"+
-	"薄雾浓云愁永昼瑞脑销金兽佳节又重阳玉枕纱橱半夜初透东篱把酒黄昏后有暗香盈袖莫道销魂帘卷西风比"+
-	"暗淡轻黄体性柔情疏迹远只香留何须浅碧深红色自是花中第一流梅定妒菊应羞画阑开冠中秋骚人可煞无情思何事当年见收"+
-	"小楼寒夜长帘幕低垂恨萧萧无情风雨夜来揉损琼肌也似贵妃醉脸也似孙寿愁眉韩令偷香徐娘傅粉莫将比拟未新奇细看取屈平陶令风韵正相宜微风起清芬酝减酴醿渐秋雪清玉瘦向无限依依似愁凝汉皋解佩似泪洒纨扇题诗朗月清风浓烟暗雨天教憔悴度芳姿纵爱惜知从此留得几多时人情好何须更忆泽畔东篱"+
-	"落熔金暮云合璧在何染柳烟浓吹梅笛春意知几许元宵佳节融和天气次第岂无风雨来相召香车宝马谢他酒朋诗侣中州盛闺门多暇记得偏重三五铺翠冠儿捻金雪柳簇带争济楚如今憔悴风鬟霜鬓见夜间出去如向帘儿底下听笑语"+
-	"寒萧萧上琐窗梧桐应恨夜来霜酒阑更喜团茶苦梦断偏宜瑞脑香秋已尽犹长仲宣怀远更凉如随分尊前醉莫负东篱菊蕊黄"
-	lastName []string
-	lastNames []string
 
+	//定义一堆名字
+	names = "纤云弄巧飞星传恨银迢迢暗风玉露一相逢胜间无数饮醒复家息雷应倚杖听江声长此何时忘却营阑风静纹平小舟从此逝江海寄余生梦" +
+		"后楼台高锁帘幕年春恨却时落花立微雨双生若只如初见何事秋风画胜寻芳泗水滨无边光景一时新等闲识得风千春江潮水连海平海明月共潮生随波千" +
+		"万里何处春江无月明江流宛转绕芳甸月照花林皆似霰空里流霜觉飞汀白沙看见江天一色无纤尘皎皎空中孤月轮江畔何初见月江月何年初照生代" +
+		"无已江月年只相似知江月待何但见长江送流水白云一去悠悠青枫浦谁家今扁舟子何相思明月楼可楼月徘徊应照离妆镜台玉户帘中卷去衣砧拂还" +
+		"此时相望相闻愿逐月华流照君鸿长飞光鱼潜跃水成文昨闲潭梦落花可春半还家江水流春去欲尽江潭落月复西斜斜月沉沉藏海雾碣石潇湘无限路知乘" +
+		"月几归落月摇情满江树院深几许杨柳烟帘幕无数玉勒鞍游冶楼高见章台路雨风三月暮门掩黄昏无计留春住泪眼问花花语乱飞过秋千去" +
+		"秋风清秋月明落叶聚还散寒复惊相思相见知何此时此难为情入我相思门知我相思苦长相思兮长相忆相思兮无极早知如此绊心何如当初莫相识" +
+		"天门中断楚江开碧流回两岸青山相对出帆明月出天山苍茫云海间长风几万里吹玉门关白登道胡青海由来征战地见有还客望边邑思归多苦颜望边楼未应闲" +
+		"中岁颇好道晚家南山陲来每独往胜事空自知行到水穷坐看云起时偶然值林叟笑无还期新丰美酒斗十千阳游年相逢意气为君饮系马高楼垂柳边出身仕汉羽林郎" +
+		"初随骠战渔阳知向边庭苦纵死犹闻侠香一身能两弧千只似无偏坐金鞍调白羽纷纷射五单于家君臣欢宴终高议云台论战功天子临轩赐侯印将佩明" +
+		"积雨空林烟火迟炊黍饷东漠水田飞白鹭阴阴夏木啭黄鹂山中习静观朝槿松下清斋折露葵与席何事更相疑尊前拟把归期说欲语春容先惨咽生自是有情痴此恨关风与月离歌且莫翻新阕一曲结直须看尽洛城花始共春风容易别" +
+		"驿外断桥边寂寞开无主已是黄昏独自愁更著风和雨无意苦争春一任群芳妒零落成泥碾作尘只有香如故早岁那知世事艰中原北望气如山楼船夜雪瓜洲渡铁马秋风大散关塞上长城空自许镜中衰鬓已先斑出师一表真名世千载谁堪伯仲" +
+		"红藕香残玉簟秋轻解罗裳独上兰舟云中谁寄锦书来雁字回月满西楼花自飘零水自流一种相思两闲愁此情无计可消除才下眉头却上心头" +
+		"薄雾浓云愁永昼瑞脑销金兽佳节又重阳玉枕纱橱半夜初透东篱把酒黄昏后有暗香盈袖莫道销魂帘卷西风比" +
+		"暗淡轻黄体性柔情疏迹远只香留何须浅碧深红色自是花中第一流梅定妒菊应羞画阑开冠中秋骚人可煞无情思何事当年见收" +
+		"小楼寒夜长帘幕低垂恨萧萧无情风雨夜来揉损琼肌也似贵妃醉脸也似孙寿愁眉韩令偷香徐娘傅粉莫将比拟未新奇细看取屈平陶令风韵正相宜微风起清芬酝减酴醿渐秋雪清玉瘦向无限依依似愁凝汉皋解佩似泪洒纨扇题诗朗月清风浓烟暗雨天教憔悴度芳姿纵爱惜知从此留得几多时人情好何须更忆泽畔东篱" +
+		"落熔金暮云合璧在何染柳烟浓吹梅笛春意知几许元宵佳节融和天气次第岂无风雨来相召香车宝马谢他酒朋诗侣中州盛闺门多暇记得偏重三五铺翠冠儿捻金雪柳簇带争济楚如今憔悴风鬟霜鬓见夜间出去如向帘儿底下听笑语" +
+		"寒萧萧上琐窗梧桐应恨夜来霜酒阑更喜团茶苦梦断偏宜瑞脑香秋已尽犹长仲宣怀远更凉如随分尊前醉莫负东篱菊蕊黄"
+	lastName  []string
+	lastNames []string
 )
 
-/*
-在包的初始化方法中，为不同姓氏配置不同辈分字切片
-这里桫哥模拟的是为欧阳氏添加“宗的永其光”五个辈分字，其余的是乱扯的~
-*/
+func GetRandomName(n int, first string) (name string) {
+	familyName := first
+	if n == 3 {
+		middleName := lastNames[GetRandomInt(0, len(lastNames)-1)]
+		lastName := lastNames[GetRandomInt(0, len(lastNames)-1)]
+		return familyName + middleName + lastName
+	} else if n == 2 {
+		lastName := lastNames[GetRandomInt(0, len(lastNames)-1)]
+		return familyName + lastName
+	} else if n == 4 {
+		lastName := lastNames[GetRandomInt(0, len(lastNames)-1)]
+		lastNamee := lastNames[GetRandomInt(0, len(lastNames)-1)]
+		return familyName + lastName + lastNamee
+	}
+	return familyName
+}
 func init() {
 	data, err := ioutil.ReadFile("shici.txt")
 	var get string
-    if err != nil {
-        fmt.Println("File reading error", err)
+	if err != nil {
+		fmt.Println("File reading error", err)
 		for _, s := range names {
-			lastNames = append(lastNames,string(s))
+			lastNames = append(lastNames, string(s))
 		}
-		
-    }else{
+
+	} else {
 		get = string(data)
 		reg := regexp.MustCompile("\\s*\\pP*")
-    	get = reg.ReplaceAllString(get,"")
+		get = reg.ReplaceAllString(get, "")
 		for _, s := range get {
-			lastNames = append(lastNames,string(s))
+			lastNames = append(lastNames, string(s))
 		}
 	}
-	
+
 	// lastNames = DeleteRepeat(lastName)
 	for _, x := range familyNames {
 		if x != "周" {
@@ -71,41 +81,8 @@ func init() {
 		}
 	}
 }
-func DeleteRepeat(list []string) []string {
-    mapdata := make(map[string]interface{})
-    if len(list) <= 0 {
-        return nil
-    }
-    for _, v := range list {
-        mapdata[v] = "true"
-    }
-    var datas []string
-    for k, _ := range mapdata {
-        if k == "" {
-            continue
-        }
-        datas = append(datas, k)
-    }
-    return datas
-}
 
-func GetRandomName(n int) (name string) {
-	familyName := familyNames[GetRandomInt(0, len(familyNames)-1)]
-	if n ==3{
-		middleName := lastNames[GetRandomInt(0, len(lastNames)-1)]
-		lastName := lastNames[GetRandomInt(0, len(lastNames)-1)]
-		return familyName +middleName + lastName
-	}else if n ==2{
-		lastName := lastNames[GetRandomInt(0, len(lastNames)-1)]
-		return familyName  + lastName
-	}else if n ==4 {
-		lastName := lastNames[GetRandomInt(0, len(lastNames)-1)]
-		lastNamee := lastNames[GetRandomInt(0, len(lastNames)-1)]
-		return familyName  + lastName + lastNamee
-	}
-	return familyName
-}
-var(
+var (
 	//随机数互斥锁（确保GetRandomInt不能被并发访问）
 	randomMutex sync.Mutex
 )
@@ -127,38 +104,42 @@ func GetRandomInt(start, end int) int {
 	randomMutex.Unlock()
 	return n
 }
-var numberFlag = flag.Int("number",100, "需要名字个数 : -number")
-var lengthFlag = flag.Int("length",3, "需要名字长度 : -length")
-func main() { 
-	flag.Parse()
-	var m string
-	for i:=0;i<*numberFlag;i++  {
-		s := GetRandomName(*lengthFlag)
-		fmt.Println(s)
-		if i % 10 !=0 {
-			m += s +"\t"
-		}else{
-			m += "\n"
-		}
-		
-	}
-	writeStringToFile("./name.txt",m)
-}
+
+//定义cmd参数
+var (
+	numberFlag    = flag.Int("number", 100, "需要名字个数 : -number")
+	lengthFlag    = flag.Int("length", 3, "需要名字长度 : -length")
+	firstNameFlag = flag.String("first", "周", "姓氏 : -first")
+)
 
 func writeStringToFile(filepath, content string) {
 	f, err := os.Create(filepath)
-    if err !=nil{
+	if err != nil {
 		fmt.Println("错误")
 	}
 
-    defer f.Close()
-    n2 , err := f.Write([]byte (content))
-    if err !=nil{
+	defer f.Close()
+	n2, err := f.Write([]byte(content))
+	if err != nil {
 		fmt.Println("错误")
 	}
-	if n2 !=0{
+	if n2 != 0 {
 		fmt.Println("写出完成：name.txt")
 	}
-	f.Sync()
 	fmt.Scanln()
- }
+}
+func main() {
+	flag.Parse()
+	var m string
+	for i := 0; i < *numberFlag; i++ {
+		s := GetRandomName(*lengthFlag, *firstNameFlag)
+		fmt.Println(s)
+		if i%10 != 0 {
+			m += s + "\t"
+		} else {
+			m += "\n"
+		}
+
+	}
+	writeStringToFile("./name.txt", m)
+}
